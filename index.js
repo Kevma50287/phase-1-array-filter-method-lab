e16a3eeb1c76b085b4149076408d08254af5371e
Kevin <magavakevin10@gmail.com>
2022-07-21 12:33:19 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+const findMatching = (array, name) => {
+    //case insensitive, simple method is to compare after lower casing both sides
+    const namematch = (element) => {
+        return element.toLowerCase() === name.toLowerCase()
+    }
+    return array.filter(namematch)
+}
+
+const fuzzyMatch = (array, name) => {
+
+    const namematch = (element) => {
+        return element[0].toLowerCase() === name[0].toLowerCase()
+    }
+
+    return array.filter(namematch)
+}
+
+//takes an array of objects and a string to match
+const matchName = (array, Name) => {
+    //need to match the obj property to the name
+    const namematch = (obj) => {
+        return obj.name === Name
+    }
+    return array.filter(namematch)
+}
